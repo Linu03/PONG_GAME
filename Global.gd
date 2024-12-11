1254ef567db9463cabe539b27f32355c91ca1067
Linu03 <163161692+Linu03@users.noreply.github.com>
2024-12-11 16:37:08 +0200

    Add files via upload

---
+++ Global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var wining_score : int = 4 # default short
+var is_vs_ai: bool = true # default vs AI
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
